8d11e39e466c990513143d21a0e93de065bb8acc
yangger <18902833810@163.com>
2017-04-21 14:20:34 +0800

    sp-list

---
+++ src/components/spList/spList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="sp-list" :class="">
+    <slot></slot>
+  </ul>
+</template>
+<style scoped rel="stylesheet/scss" lang="scss">
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        }
+    }
+</script>
